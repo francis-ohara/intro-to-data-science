db21e7094fa9a37080c812b990f3423d271b8b11
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2022-10-29 20:18:29 +0000

    Update 5: matplotlib exercises

---
+++ main.py
@@ -0,0 +1,10 @@
+# import matplotlib.pyplot as plt
+#
+#
+# plt.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+# plt.show()
+import pandas as pd
+
+x = pd.DataFrame([[1, "hello", 44.5], [2, "world", 50.0], [3, "me", 66.7]])
+print(x)
+print(x[1])
