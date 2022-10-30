11d65fd8adfe476b1c5a5d2825ed46e55ba2ccf1
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2022-10-30 21:20:55 +0000

    Update 5: matplotlib exercises

--- main.py
+++ main.py
@@ -3,8 +3,21 @@
 #
 # plt.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 # plt.show()
-import pandas as pd
 
-x = pd.DataFrame([[1, "hello", 44.5], [2, "world", 50.0], [3, "me", 66.7]])
-print(x)
-print(x[1])
+# import pandas as pd
+#
+# x = pd.DataFrame([[1, "hello", 44.5], [2, "world", 50.0], [3, "me", 66.7]])
+# print(x)
+# print(x[1])
+
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+
+samples, clusters = make_blobs(centers=4)
+
+x_values = samples[:, 0]
+y_values = samples[:, 1]
+
+plt.scatter(x_values, y_values, c=clusters)
+plt.show()
+
